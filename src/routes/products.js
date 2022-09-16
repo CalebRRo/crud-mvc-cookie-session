31788762bd1d3e2ef13157bb8fb1b4d6cea7d5e3
CalebRR <yohannagraciano@gmail.com>
2022-09-16 10:30:46 -0300

    add config sequelize

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
+
+
 // ************ Middlewares Require ************
 
 const {uploadImageProduct} = require("../middlewares/uploadFiles")
