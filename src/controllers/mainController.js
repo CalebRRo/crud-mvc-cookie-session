a55f6db432513fec13542b45b7d9ccb9f9b0761e
CalebRR <yohannagraciano@gmail.com>
2022-10-02 17:55:11 -0300

    read product done!

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,15 +1,13 @@
 const db = require("../database/models")
 const {Op} = require("sequelize")
-const {loadProducts} = require("../data/productsModule")
+
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
 	index: (req, res) => {
 		// Do the magic
-/* 		const products = loadProducts()
-		const inSale = products.filter(product => product.category === "in-sale")
-		const visited = products.filter(product => product.category === "visited") */
+
       let inSale = db.Product.findAll({
 		where : {
             discount : {
@@ -49,14 +47,43 @@ const controller = {
 	},
 	search: (req, res) => {
 		// Do the magic
-		const products = loadProducts();
+	/*  	const products = loadProducts();
 		const result = products.filter(product => product.name.toLowerCase().includes(req.query.keywords.toLowerCase()))	;
 
 		return res.render("results",{
 			products : result,
 			keywords : req.query.keywords,
 			toThousand
+		})  */
+	 	const {keywords} = req.query;
+
+		db.Product.findAll({
+			where : {
+			[Op.or]: [
+				{
+					name : {
+					[Op.substring]: keywords
+				}
+			},
+				{
+					description: {
+					[Op.substring]: keywords
+				}
+			}
+			]				
+			},
+			include : ["images"]
 		})
+		    .then(products => {
+				return res.render("results",{
+					products,
+					keywords,
+					toThousand
+			})
+		})
+		
+			.catch(error => console.log(error)) 
+		
 	},
 	login: (req,res) => {
 		return res.render("login")
